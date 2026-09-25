0467a94cd06079db4f11a21c0ba3a5538078e5cf
remi-delmas-3000 <remi-delmas-3000@users.noreply.github.com>
2026-09-25 21:47:03 +0000

    deploy: b91df3d2d3e40f9c38711526b05f39653f1e3ece

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-30a1758b.js',
+                path_to_root + 'searchindex-1788232c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
